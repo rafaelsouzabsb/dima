f451b5b4796c16acedba25312dc7f99a8705fe03
Rafael Santos de Souza <rafael.souza.bsb@hotmail.com>
2024-10-03 19:59:40 -0300

    Organizando o Minimal API

---
+++ Dima.Api/Common/Api/AppExtension.cs
@@ -0,0 +1,23 @@
+﻿using Dima.Api.Endpoints;
+using Dima.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Dima.Api.Common.Api
+{
+    public static class AppExtension
+    {
+        public static void ConfigureDevEnironment(this WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapSwagger().RequireAuthorization();
+        }
+
+        public static void UseSecurity(this WebApplication app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,87 +1,27 @@
-using Dima.Api.Data;
+using Dima.Api;
+using Dima.Api.Common.Api;
 using Dima.Api.Endpoints;
-using Dima.Api.Handlers;
-using Dima.Api.Models;
-using Dima.Core.Handlers;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
-
-        builder.Services
-            .AddAuthentication(IdentityConstants.ApplicationScheme)
-            .AddIdentityCookies();
-        builder.Services.AddAuthorization();
-
-        var connString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
-
-        builder.Services.AddDbContext<AppDbContext>(x =>
-        {
-            x.UseSqlServer(connString);
-        });
-
-        builder.Services
-            .AddIdentityCore<User>()
-            .AddRoles<IdentityRole<long>>()
-            .AddEntityFrameworkStores<AppDbContext>()
-            .AddApiEndpoints();
-
-        builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
-        builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+        builder.AddConfiguration();
+        builder.AddSecurity();
+        builder.AddDataContext();
+        builder.AddCrossOrigin();
+        builder.AddDocumentation();
+        builder.AddServices();
 
         var app = builder.Build();
 
-        app.UseAuthentication();
-        app.UseAuthorization();
+        if (app.Environment.IsDevelopment())
+            app.ConfigureDevEnironment();
 
-        app.UseSwagger();
-        app.UseSwaggerUI();
-
-        app.MapGet("/", () => new { messsage = "OK" });
+        app.UseCors(ApiConfiguration.CorsPolicyName);
+        app.UseSecurity();
         app.MapEndpoints();
-        app.MapGroup("v1/identity")
-            .WithTags("Identity")
-            .MapIdentityApi<User>();
-
-        app.MapGroup("v1/identity")
-         .WithTags("Identity")
-         .MapPost("/logout", async (
-             SignInManager<User> signInManager) =>
-         {
-             await signInManager.SignOutAsync();
-             return Results.Ok();
-         })
-         .RequireAuthorization();
-
-        app.MapGroup("v1/identity")
-        .WithTags("Identity")
-        .MapPost("/roles", (
-            ClaimsPrincipal user) =>
-        {
-            if (user.Identity is null || !user.Identity.IsAuthenticated)
-                return Results.Unauthorized();
-
-            var identity = (ClaimsIdentity)user.Identity;
-            var roles = identity.FindAll(identity.RoleClaimType).Select(c => new
-            {
-                c.Issuer,
-                c.OriginalIssuer,
-                c.Type,
-                c.Value,
-                c.ValueType
-            });
-
-            return TypedResults.Json(roles);
-        })
-        .RequireAuthorization();
 
         app.Run();
     }
